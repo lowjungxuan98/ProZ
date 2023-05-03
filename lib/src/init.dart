676a2e5c8e3f4e6c8de3043e9219097bdc334a56
LowJungXuan6157 <124644086+LowJungXuan6157@users.noreply.github.com>
2023-05-03 23:47:26 +0800

    StreamingSharedPreferences init

--- lib/src/init.dart
+++ lib/src/init.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
 import 'package:pro_z/src/store/store_index.dart';
+import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
+
+import 'services/services.dart';
 
 class ProZ {
   static Future init() async {
+    final preference = await StreamingSharedPreferences.instance;
+    Get.put<StorageService>(StorageService(preference));
     Get.put<HttpSetting>(HttpSetting());
   }
 }
