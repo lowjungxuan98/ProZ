59881a3cc513b4181b676f9e490000bd7930e75c
LowJungXuan6157 <124644086+LowJungXuan6157@users.noreply.github.com>
2023-05-05 14:23:51 +0800

    http bug fix

--- lib/src/store/http_setting.dart
+++ lib/src/store/http_setting.dart
@@ -4,20 +4,15 @@ import '../services/services.dart';
 
 class HttpSetting extends GetxController {
   static HttpSetting get to => Get.find();
-  final RxString _token = ''.obs;
-  set token(value) => _token.value = value;
-  String get token => _token.value;
-  final RxString _baseUrl = ''.obs;
-  set baseUrl(value) => _baseUrl.value = value;
-  String get baseUrl => _baseUrl.value;
+  String get token => StorageService.to.getString('STORAGE_USER_TOKEN_KEY');
+  String get baseUrl => StorageService.to.getString('HTTP_SETTING_BASE_URL');
   /// 保存 token
   Future<void> setToken(String value) async {
     await StorageService.to.setString('STORAGE_USER_TOKEN_KEY', value);
-    token = value;
   }
   Future<void> setBaseUrl(String value) async {
-    _baseUrl.value = value;
-    _baseUrl.refresh();
+    // _baseUrl.value = value;
+    // _baseUrl.refresh();
     await StorageService.to.setString('HTTP_SETTING_BASE_URL', value);
   }
 }
